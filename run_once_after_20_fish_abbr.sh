8932a1616117b8f515f4851703a6434c9f41aa5b
Ivan Yastrebov <easy.quest@vk.com>
2021-12-25 13:55:13 +0300

    first commit

---
+++ run_once_after_20_fish_abbr.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env fish
+
+for abbreviation in (abbr -l)
+    abbr -e $abbreviation
+end
+abbr g 'git '
+abbr gf 'git fetch '
+abbr gs 'git status '
+abbr gc 'git commit -m '
+abbr md 'mkdir -p '
+abbr pi '.venv/bin/python -m pip install -U '
+abbr rm 'rm -Rf '
+abbr xx 'chmod +x '
+abbr z 'chezmoi '
+abbr ze 'chezmoi edit --apply '
+abbr pi ".venv/bin/python -m pip install -U "
+abbr m "micro "
