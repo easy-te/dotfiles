0ac6c16b6b234ea21ba605d1936dd04cd84b5f22
Ivan Yastrebov <easy.quest@vk.com>
2022-01-05 14:05:19 +0000

    Update dot_zshrc Add run_once_after_20_zsh_alias.sh

---
+++ run_once_after_20_zsh_alias.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#for aliaseviation in (alias -l)
+#    alias -e $abbreviation
+
+alias g='git '
+alias gf='git fetch '
+alias gs='git status '
+alias gc='git commit -m '
+alias md='mkdir -p '
+alias pi='.venv/bin/python -m pip install -U '
+alias rm='rm -Rf '
+alias xx='chmod +x '
+alias z='chezmoi '
+alias ze='chezmoi edit --apply '
+alias pi=".venv/bin/python -m pip install -U "
+alias m="micro "
+alias sshd="sudo /sbin/sshd -h ~/.ssh/id_rsa;ip addres |grep 192.168."
